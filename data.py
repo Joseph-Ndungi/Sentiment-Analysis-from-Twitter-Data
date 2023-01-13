5242898ad78c271ddd88c93fa008c72d98fd0988
Joseph Ndungi <josendungi4@gmail.com>
2023-01-13 20:28:33 +0300

    paginator to flatten

--- data.py
+++ data.py
@@ -1,14 +1,22 @@
-import snscrape.modules.twitter as sntwitter
+# OAuth2.0 Version 
+import tweepy
 import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-query = "(twitter) -is:retweet lang:en)"
-tweets = []
-limit = 5000
+#Put your Bearer Token in the parenthesis below
+client = tweepy.Client(bearer_token=os.environ["Bearer_Token"])
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
-    if i > limit:
-        break
-    tweets.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
 
-df = pd.DataFrame(tweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
-df.to_csv('tweets.csv', index=False)
+query = '(twitter) -is:retweet lang:en'
+tweets = tweepy.Paginator(client.search_recent_tweets, query=query,
+                              tweet_fields = ["author_id","text","geo","public_metrics","possibly_sensitive",
+                                    #"promoted_metrics","organic_metrics","non_public_metrics"
+                                    "referenced_tweets","reply_settings","source","withheld"], max_results=100).flatten(limit=1000)
+
+import itertools
+
+# Flatten the tweets into a list
+tweets_list = list(itertools.chain(tweets))
+print(tweets_list)
