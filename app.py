9803fbdf1c894b80d61dfe7e79bc673a62bc5a4c
Joseph Ndungi <josendungi4@gmail.com>
2023-02-14 17:50:48 +0300

    cardiffnlp/twitter-roberta-base-sentiment-latest

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import tweepy as tw
 from dotenv import load_dotenv
 import pandas as pd
-#import tweepy
+import json
 
 
 #import time
@@ -37,12 +37,81 @@ tweets_data = tweets_dict['data']
 # Transform to pandas Dataframe
 df = pd.json_normalize(tweets_data) 
 # print(df.head())
+df= df['text']
+#remove special characters
+df = df.str.replace('[^a-zA-Z0-9\s]', '')
 
-# Save to csv
-df.to_csv('app.csv', index = False)
+#remove stopwords
+'''
+TODO:
+ Text Preprocessing
+ Use roberta model to predict the sentiment of the tweets
+'''
+
+# from nltk.corpus import stopwords
+# stop = stopwords.words('english')
+# df = df.apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+STOPWORDS = set(stopwordlist)
+df = df.apply(lambda x: " ".join(x for x in x.split() if x not in STOPWORDS))
+
+#save csv
+df.to_csv('cleaned.csv', index = False)
+
+
+
+
+#make it json serializable
+df = df.to_json(orient='records')
+df = json.loads(df)
+# print(df)
+
+# # Save to csv
+# df.to_csv('app.csv', index = False)
 
 '''
 TODO:
  Text Preprocessing
  Use roberta model to predict the sentiment of the tweets
 '''
+
+#from df read text column
+
+
+
+
+model = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+hf_token = "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"
+
+
+API_URL = "https://api-inference.huggingface.co/models/" + model
+headers = {"Authorization": "Bearer %s" % (hf_token)}
+
+def analysis(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+
+
+payload = {"inputs": df}
+response = analysis(payload)
+print(response)
+
+#append the response to the df with the keys as the columns and the values as the rows of the columns
+
