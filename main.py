3fede1a05644689a7c555f059df9f2dc342ca581
Joseph Ndungi <josendungi4@gmail.com>
2023-04-07 14:59:44 +0300

    analysis

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import json
 import pandas as pd
 from forms import *
+import csv
+import json
+import itertools
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from wordcloud import STOPWORDS
+import tweepy
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import plotly
+import plotly.express as px
+import base64
+from io import BytesIO
+from textblob import TextBlob
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+import numpy as np
+
+
+
+client = tweepy.Client(bearer_token=os.environ["Bearer_Token"])
+
 
 
 app = Flask(__name__)
@@ -16,20 +45,295 @@ def index():
     #     print(form.endDate.data)
     #     print(form.query.data)
     #     return redirect(url_for('index'))
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', title='Dashboard')
 
 
 
 @app.route('/sentiments', methods=['GET', 'POST'])
 def sentiments():
+    
+    form = DateForm()
+    # graph1 = ''
+    # wordcloud = []
+    if request.method == 'POST':
+    #if form.validate_on_submit():
+        query = form.query.data + ' -is:retweet lang:en'
+        tweets = tweepy.Paginator(client.search_recent_tweets, query=query,
+                              tweet_fields = ["author_id","text","geo","public_metrics","possibly_sensitive",
+                                    #"promoted_metrics","organic_metrics","non_public_metrics"
+                                    "referenced_tweets","reply_settings","source","withheld"], max_results=100).flatten(limit=1000)
+        
+        tweets_list = list(itertools.chain(tweets))
 
-    # form = DateForm()
-    # if form.validate_on_submit():
-    #     print(form.startDate.data)
-    #     print(form.endDate.data)
-    #     print(form.query.data)
-    #     return redirect(url_for('index'))
-    return render_template('query.html')
+        tweets_dict = [dict((k, v) for k, v in d.items() if k != 'id' and k != 'edit_history_tweet_ids') for d in tweets_list]
+
+        df = pd.json_normalize(tweets_dict)
+
+        df= df['text']
+        df = df.str.replace('[^a-zA-Z0-9\s]', '')
+        stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an','http'
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+        STOPWORDS = set(stopwordlist)
+        df = df.apply(lambda x: " ".join(x for x in x.split() if x not in STOPWORDS))
+        dfData=df
+        df = df.to_json(orient='records')
+        df = json.loads(df)
+
+
+        hf_token = "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"
+        model="cardiffnlp/twitter-xlm-roberta-base-sentiment"
+
+        API_URL = "https://api-inference.huggingface.co/models/" + model
+        headers = {"Authorization": "Bearer %s" % (hf_token)}
+
+
+        def analysis(payload):
+            data = json.dumps(payload)
+            response = requests.request("POST", API_URL, headers=headers, data=data)
+            return json.loads(response.content.decode("utf-8"))
+
+
+        payload = {"inputs": df}
+        response = analysis(payload)
+
+        jsonFile= response
+        df = pd.DataFrame(columns=['label', 'score'])
+        #df.head()
+        for lst in jsonFile:  
+            maxScore = max(lst, key=lambda d: d['score'])
+            # df = df.append({'label': maxScore['label'], 'score': maxScore['score']}, ignore_index=True)
+            df = pd.concat([df, pd.DataFrame({'label': maxScore['label'], 'score': maxScore['score']}, index=[0])], ignore_index=True)
+
+        
+        df = pd.concat([df, dfData], axis=1)
+        #df to csv
+        df.to_csv('sentiment.csv', index = False)
+
+        sentiment_counts = df.groupby(['label']).size()
+        print(sentiment_counts)
+
+        #visualize in a pie chart
+        fig= px.pie(df, values=sentiment_counts, names=sentiment_counts.index, title='Sentiment Analysis')
+        graph1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+        
+        # sentiment_counts.plot(kind='pie', autopct='%1.0f%%')
+        # plt.show()
+
+        positive_tweets = df['text'][df["label"] == 'positive']
+        #positive_tweets.to_csv('positivetweets.csv', index = False)
+
+        stop_words = ["https", "co", "RT"] + list(STOPWORDS)
+
+        wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black', colormap='Set2', collocations=False, stopwords = stop_words, max_words = 200).generate(str(positive_tweets))
+        #use px to create a plotly figure of the wordcloud
+        fig = px.imshow(wordcloud)
+        fig.update_layout(
+            title="Wordcloud of Positive Tweets",
+            xaxis_title="Word",
+            yaxis_title="Frequency",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="#7f7f7f"
+            )
+        )
+        #fig.show()
+        #dump the plotly figure into a json string
+        fig = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        #negative_tweets wordcloud
+        negative_tweets = df['text'][df["label"] == 'negative']
+        wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black', colormap='Set2', collocations=False, stopwords = stop_words, max_words = 200).generate(str(negative_tweets))
+        #use px to create a plotly figure of the wordcloud
+        fig = px.imshow(wordcloud)
+        fig.update_layout(
+            title="Wordcloud of Positive Tweets",
+            xaxis_title="Word",
+            yaxis_title="Frequency",
+            font=dict(
+                family="Courier New, monospace",
+                size=18,
+                color="#7f7f7f"
+            )
+        )
+        #fig.show()
+        #dump the plotly figure into a json string
+        fig1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        #neutral_tweets wordcloud
+        neutral_tweets = df['text'][df["label"] == 'neutral']
+        wordcloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black', colormap='Set2', collocations=False, stopwords = stop_words, max_words = 200).generate(str(neutral_tweets))
+        #use px to create a plotly figure of the wordcloud
+        fig = px.imshow(wordcloud)
+        fig.update_layout(
+            title="Wordcloud of Neutral Tweets",
+            xaxis_title="Word",
+            yaxis_title="Frequency",
+            font=dict(
+                
+                family="Courier New, monospace",
+                size=18,
+                color="#7f7f7f"
+            )
+        )
+
+        #dump the plotly figure into a json string
+        fig2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        
+
+        return render_template('query.html', form=form, graph1=graph1, wordcloud=fig,
+                               wordcloud1=fig1, wordcloud2=fig2, title='Sentiment Analysis')
+
+
+    
+    return render_template('query.html', form=form,title='Sentiment Analysis')
+
+@app.route('/analysis', methods=['GET', 'POST'])
+def analysis():
+    #open the csv file
+    df = pd.read_csv('sentiment.csv')
+
+    #get total number of positive, negative and neutral tweets each
+    positive_tweets = df['text'][df["label"] == 'positive']
+    negative_tweets = df['text'][df["label"] == 'negative']
+    neutral_tweets = df['text'][df["label"] == 'neutral']
+
+    #get the totals of each
+    positive_tweets_count = positive_tweets.count()
+    negative_tweets_count = negative_tweets.count()
+    neutral_tweets_count = neutral_tweets.count()
+
+    # #visualize the sentiment distribution using a histogram
+    # fig = px.histogram(df, x="label", title="Sentiment Distribution")
+    # fig.update_layout(
+    #     title="Sentiment Distribution",
+    #     xaxis_title="Sentiment",
+    #     yaxis_title="Frequency",
+    #     font=dict(
+    #         family="Courier New, monospace",
+    #         size=18,
+    #         color="#7f7f7f"
+    #     )
+    # )
+    # #dump the plotly figure into a json string
+    # fig = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    #visualize the sentiment distribution using boxplot
+    fig2 = px.box(df, x="label", y="score", title="Sentiment Distribution")
+    fig2.update_layout(
+        title="Sentiment Distribution",
+        xaxis_title="Sentiment",
+        yaxis_title="Score",
+        font=dict(
+            
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"
+        )
+    )
+    #dump the plotly figure into a json string
+    fig2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # calculate the sentiment polarity of each tweet
+    df['sentiment_polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+
+    #visualize the sentiment polarity distribution using a histogram
+    fig3 = px.histogram(df, x="sentiment_polarity", title="Sentiment Polarity Distribution")
+
+    fig3.update_layout(
+        title="Sentiment Polarity Distribution",
+        xaxis_title="Sentiment Polarity",
+        yaxis_title="Frequency",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"
+        )
+    )
+
+    #dump the plotly figure into a json string
+    fig3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    '''
+    We then create a document-term matrix using the CountVectorizer from scikit-learn,
+      which converts each tweet into a vector of word frequencies. We fit an LDA model to this matrix,
+        with 10 topics. Finally, we print the top 10 most common words for each topic, and visualize the
+          distribution of topics in the tweets using a histogram.
+    '''
+
+    #define stop words
+    stop_words = set(stopwords.words('english'))
+
+    # tokenize the tweet text and remove stopwords
+    tokenized_tweets = []
+    for tweet in df['text']:
+        tokens = word_tokenize(tweet.lower())
+        filtered_tokens = [token for token in tokens if token not in stop_words]
+        tokenized_tweets.append(' '.join(filtered_tokens))
+
+    #create a document-term matrix  
+    vectorizer = CountVectorizer(max_features=1000)
+    doc_term_matrix = vectorizer.fit_transform(tokenized_tweets)
+    # fit an LDA model to the document-term matrix
+    lda_model = LatentDirichletAllocation(n_components=10, max_iter=10, learning_method='online', random_state=0)
+    lda_model.fit(doc_term_matrix)
+
+    topic_words = {}
+    message = ""
+
+    for i, topic in enumerate(lda_model.components_):
+        word_idx = np.argsort(topic)[::-1][:10]
+        topic_words[i] = [vectorizer.get_feature_names_out()[i] for i in word_idx]
+        #print('Topic {}: {}'.format(i, ' '.join(topic_words[i])))
+        #message += 'Topic {}: {}'.format(i, ' '.join(topic_words[i]))
+        line = 'Topic {}: {}\n'.format(i, ' '.join(topic_words[i]))
+        message += line
+ 
+
+
+    #plot the distribution of topics in the tweets
+    fig4 = px.histogram(lda_model.transform(doc_term_matrix), title="Topic Distribution")
+    fig4.update_layout(
+        title="Topic Distribution",
+        xaxis_title="Topic",
+        yaxis_title="Frequency",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"
+        )
+    )
+
+    #dump the plotly figure into a json string
+    fig4 = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return render_template('analysis.html',
+                            fig2=fig2,fig3=fig3,fig4=fig4,
+                             title='Analysis',
+                             tPos=positive_tweets_count,
+                             tNeg=negative_tweets_count,
+                             tNeu=neutral_tweets_count,
+                             message=message)
 
 if __name__ == '__main__':
     app.run(debug=False)
--- model.py
+++ model.py
@@ -1,35 +1,27 @@
 import pandas as pd
+import numpy as np
 
 # Clean Data
-data = pd.read_csv("Variables3.csv")
+clean_data = pd.read_csv("Variables3.csv")
 
-def tag_location(location):
-    if location:
-        return "Tagged"
-    else:
-        return "Non tagged"
 
-# Drop variables
-clean_data = data
-#clean_data["Text"] = pd.to_numeric(clean_data["Text"])
-clean_data["VerifiedRetweet"] = pd.to_numeric(clean_data["VerifiedRetweet"])
-# clean_data["Characters"] = pd.to_numeric(clean_data["Characters"])
-clean_data["Verified"] = pd.to_numeric(clean_data["Verified"])
-clean_data["Protected"] = pd.to_numeric(clean_data["Protected"])
-#clean_data["Location"] = pd.to_numeric(clean_data["Location"])
-# clean_data["Location"] = ["TAGGED" if i >= 2 else "NON-TAGGED" for i in clean_data["Location"]]  # set geotags to true or false
-# clean_data["Followers"] = [1 if i >= 500 else 0 for i in clean_data["Followers"]]  # if number of followers is greater than 500 then TRUE
+# def tag_status(booleanStatus):
+#     if booleanStatus == True:
+#         return 1
+#     else:
+#         return 0
+    
+# clean_data["Verified"] = clean_data["Verified"].apply(tag_status)
+# clean_data["Protected"] = clean_data["Protected"].apply(tag_status)
+# clean_data["VerifiedRetweet"] = clean_data["VerifiedRetweet"].apply(tag_status)
 clean_data["VerifiedRetweet"].fillna(1, inplace=True)  # Remove NA values
-# clean_data.fillna(1, inplace=True)  # Convert NA values to 1
 
-# print(clean_data.head())
-# clean_data["Location"] = clean_data["Location"].apply(tag_location)
 
 def tag_location(location):
     if isinstance(location, str) and location.strip():
-        return 'Tagged'
+        return "Tagged"
     else:
-        return 'Non Tagged'
+        return "Non-Tagged"
     
 
 clean_data["Location"] = clean_data["Location"].apply(tag_location)
@@ -37,20 +29,34 @@ clean_data["Location"] = clean_data["Location"].apply(tag_location)
 #check the followers column for values greater than 500
 def tag_followers(followers):
     if followers >= 500:
-        return 1
+        return True
     else:
-        return 0
+        return False
     
 clean_data["Followers"] = clean_data["Followers"].apply(tag_followers)
 
+#count characters in the text column
+def count_characters(text):
+    if isinstance(text, str):
+        return len(text)
+    else:
+        return 0
+    
+clean_data["Character"] = clean_data["Text"].apply(count_characters)
+
+def tag_characters(characters):
+    if characters >= 100:
+        return True
+    else:
+        return False
+
+
+clean_data["Character"] = clean_data["Character"].apply(tag_characters)
+
+
+
 clean_data.fillna(1, inplace=True)  # Convert NA values to 1
 
-# SUBSETTING DATA TO TRAIN& TEST SETS
-# Split data into train and test sets
-#train = clean_data.sample(frac=0.8, random_state=1)
-
-import pandas as pd
-import numpy as np
 
 def create_train_test(data, size=0.8, train=True):
     n_row = data.shape[0]
@@ -61,63 +67,142 @@ def create_train_test(data, size=0.8, train=True):
     else:
         return data.iloc[-train_sample,:]
 
-clean_data1 = clean_data.drop(["Protected"], axis=1) # remove unwanted variables
-data_train = create_train_test(clean_data1, 0.8, train=True)
-data_test = create_train_test(clean_data1, 0.8, train=False)
-print(data_train.shape) # check dimensions for train data
-print(data_test.shape) # check dimensions for test data
+clean_data1 = clean_data.drop(["Text"], axis=1) # remove unwanted variables
+clean_data1 = clean_data1.drop(["Source"], axis=1) # remove unwanted variables
+clean_data1 = clean_data1.drop(["Unnamed: 0"], axis=1) # remove unwanted variables
 
-# print(data_train.head())
+#move the target variable to the last column
+cols = list(clean_data1.columns.values)
+cols.pop(cols.index('Location'))
+clean_data1 = clean_data1[cols+['Location']]
+print(clean_data1.head())
+# data_train = create_train_test(clean_data1, 0.8, train=True)
+# print(data_train.shape) # check dimensions for train data
 
-# Model Training
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
 
-# Create Decision Tree classifer object
-clf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+# from sklearn.tree import DecisionTreeClassifier, plot_tree
+# import matplotlib.pyplot as plt
 
-# Train Decision Tree Classifer
-clf = clf.fit(data_train.drop(columns=['Location']), data_train['Location'])
 
-#Predict the response for test dataset
-y_pred = clf.predict(data_test.drop(columns=['Location']))
+# define features and target variable
+# features = data_train.columns[:-1] # all columns except the last one
+# target = data_train.columns[-1] # last column
 
-# Model Evaluation
-print("Accuracy:", accuracy_score(data_test['Location'], y_pred))
-print("Confusion Matrix:", confusion_matrix(data_test['Location'], y_pred))
-print("Classification Report:", classification_report(data_test['Location'], y_pred))
 
-# Visualize Decision Tree
-from sklearn import tree
+
+# # create and fit the decision tree model
+# model = DecisionTreeClassifier()
+# model.fit(data_train[features], data_train[target])
+
+# # visualize the decision tree using plot_tree
+# plt.figure(figsize=(20,10))
+# plot_tree(model, filled=True, feature_names=features, class_names=model.classes_)
+# plt.show()
+
+X = clean_data1[['Verified', 'Protected', 'Followers', 'VerifiedRetweet', 'Character']]
+y = clean_data1['Location']
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(25,20))
-_ = tree.plot_tree(clf,
-                     feature_names=data_train.drop(columns=['Location']).columns,
-                        class_names=data_train['Location'].unique(),
-                        filled=True)
+# split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Save Decision Tree
+# # create a decision tree model
+# dt = DecisionTreeClassifier(random_state=42)
+
+# # define the hyperparameters and their possible values
+# param_grid = {
+#     'criterion': ['gini', 'entropy'],
+#     'max_depth': [3, 5, 7, 10],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4]
+# }
+
+# # perform grid search to find the best hyperparameters
+# grid_search = GridSearchCV(dt, param_grid, cv=5)
+# grid_search.fit(X_train, y_train)
+
+# # print the best hyperparameters and the corresponding score
+# print('Best hyperparameters:', grid_search.best_params_)
+# print('Best score:', grid_search.best_score_)
+
+# # fit the model using the best hyperparameters
+# dt_best = grid_search.best_estimator_
+# dt_best.fit(X_train, y_train)
+
+# # evaluate the performance of the model on the test set
+# accuracy = dt_best.score(X_test, y_test)
+# print('Accuracy:', accuracy)
+
+# # visualize the decision tree using plot_tree
+# plt.figure(figsize=(20,10))
+# plot_tree(dt_best, filled=True, feature_names=X_train.columns, class_names=dt_best.classes_)
+# plt.show()
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+# Define the parameter grid for hyperparameter tuning
+param_grid = {
+    'n_estimators': [100, 500, 1000],
+    'max_depth': [10, 20, 30, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'class_weight': ['balanced', 'balanced_subsample', None]
+}
+
+# Create a Random Forest classifier object
+rf_model = RandomForestClassifier(random_state=42)
+
+# Perform a grid search to find the best hyperparameters
+grid_search = GridSearchCV(rf_model, param_grid, cv=5, n_jobs=-1, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+# Print the best hyperparameters
+print("Best parameters: ", grid_search.best_params_)
+print("Best score: ", grid_search.best_score_)
+
+# Use the best model to make predictions on the test set
+best_rf_model = grid_search.best_estimator_
+y_pred = best_rf_model.predict(X_test)
+
+# Evaluate the performance of the model on the test set
+accuracy = best_rf_model.score(X_test, y_test)
+print('Accuracy:', accuracy)
+
+# Print the confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+# Print the classification report
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+# Print the feature importances
+print(best_rf_model.feature_importances_)
+
+# Plot the feature importances
+plt.figure(figsize=(10,5))
+plt.bar(X_train.columns, best_rf_model.feature_importances_)
+plt.show()
+
+#save the model
 import pickle
-with open('model.pkl', 'wb') as f:
-    pickle.dump(clf, f)
+filename = 'finalized_model.sav'
+pickle.dump(best_rf_model, open(filename, 'wb'))
 
 
 
 
 
 
-# import pandas as pd
-# import numpy as np
-# from sklearn.tree import DecisionTreeClassifier
-# import matplotlib.pyplot as plt
-# from sklearn.tree import export_text
 
-# #data_train = pd.read_csv('data_train.csv') # read in your data file
 
-# X_train = data_train.drop(columns=['Location']) # features
-# y_train = data_train['Location'] # target variable
 
 
